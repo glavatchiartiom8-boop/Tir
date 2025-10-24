cc5c5216e311addc66db71cc3d87c3ccede3d12b
glavatskyy <glavatchi.artiom8@gmail.com>
2025-10-24 21:41:16 +0300

    add cycle-code

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Tir')
 icon = pygame.image.load('tir.png')
 pygame.display.set_icon(icon)
-target_img = pygame.image.load('target-sport.jpg')
+target_img = pygame.image.load('apple.png')
+target_img = pygame.transform.scale(target_img, (120, 80))
 target_width = 80
 target_height = 80
 target_x = random.randint(0, 720 - target_width)
 target_y = random.randint(0, 520 - target_height)
 
-color = (0, 0, 0)
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
@@ -21,6 +22,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + 120 and target_y < mouse_y < target_y + 80:
+                target_x = random.randint(0, 720 - target_width)
+                target_y = random.randint(0, 520 - target_height)
+
+    screen.fill(color)
+
     screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
